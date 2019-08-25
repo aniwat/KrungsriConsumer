c74c37b76a75e1b1244d92f0781a5233c2839b5b
Unknown <aniwat_keereena@hotmail.com>
2019-08-25 22:52:12 +0700

    Initial Eclipse project

---
+++ src/main/java/com/krungsri/backend/api/KrungsriConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.krungsri.backend.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrungsriConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrungsriConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/krungsri/backend/api/KrungsriConsumerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.krungsri.backend.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KrungsriConsumerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
